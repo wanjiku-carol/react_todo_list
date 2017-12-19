ea44939aa24830698d8d714ca0be7332dbcc1184
Caroline Wanjiku <Caroline Wanjiku>
2017-12-19 11:59:49 +0300

    ft(tests)Enable enzyme renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+/* global it, expect */
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
